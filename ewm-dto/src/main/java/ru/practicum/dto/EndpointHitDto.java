03f0f995ce27a91bad632281bb302683e85c7fac
Sofya Salova <ssalova04@gmail.com>
2026-07-17 13:08:15 +0300

    fix: add JsonFormat to timestamp in EndpointHitDto

--- ewm-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ ewm-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,12 +1,13 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -26,5 +27,6 @@ public class EndpointHitDto {
     private String ip;
 
     @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
