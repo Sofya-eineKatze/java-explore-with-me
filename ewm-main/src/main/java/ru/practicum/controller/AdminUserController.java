bbac5b7a73705747ba9825a5a4a8bb1e78e178cf
Sofya Salova <ssalova04@gmail.com>
2026-07-20 17:11:01 +0300

    Implement events service and fix event publishing flow

--- ewm-main/src/main/java/ru/practicum/controller/AdminUserController.java
+++ ewm-main/src/main/java/ru/practicum/controller/AdminUserController.java
@@ -1,4 +1,41 @@
 package ru.practicum.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.UserDto;
+import ru.practicum.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/users")
+@RequiredArgsConstructor
+@Slf4j
 public class AdminUserController {
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto addUser(@Valid @RequestBody UserDto userDto) {
+        log.info("POST /admin/users - add user: {}", userDto.getEmail());
+        return userService.addUser(userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long userId) {
+        log.info("DELETE /admin/users/{} - delete user", userId);
+        userService.deleteUser(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> getUsers(@RequestParam(required = false) List<Long> ids,
+                                  @RequestParam(defaultValue = "0") int from,
+                                  @RequestParam(defaultValue = "10") int size) {
+        log.info("GET /admin/users - get users with ids: {}, from: {}, size: {}", ids, from, size);
+        return userService.getUsers(ids, from, size);
+    }
 }
