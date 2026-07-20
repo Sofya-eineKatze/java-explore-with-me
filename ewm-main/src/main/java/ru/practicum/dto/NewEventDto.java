678f6ff52ab9dd68f12a79068816cd685dc5c3b0
Sofya Salova <ssalova04@gmail.com>
2026-07-20 17:40:17 +0300

    Add event validation and fix API constants

--- ewm-main/src/main/java/ru/practicum/dto/NewEventDto.java
+++ ewm-main/src/main/java/ru/practicum/dto/NewEventDto.java
@@ -1,6 +1,10 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,22 +19,43 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class NewEventDto {
 
+    @NotBlank
+    @Size(min = Constants.MIN_TITLE_LENGTH,
+            max = Constants.MAX_TITLE_LENGTH)
+    private String title;
+
+
+    @NotBlank
+    @Size(min = Constants.MIN_ANNOTATION_LENGTH,
+            max = Constants.MAX_ANNOTATION_LENGTH)
     private String annotation;
 
-    private Long category;
 
+    @NotBlank
+    @Size(min = Constants.MIN_DESCRIPTION_LENGTH,
+            max = Constants.MAX_DESCRIPTION_LENGTH)
     private String description;
 
+
+    @NotNull
+    private Long category;
+
+
+    @NotNull
+    @Future
     @JsonFormat(pattern = Constants.DATE_TIME_FORMAT)
     private LocalDateTime eventDate;
 
+
+    @NotNull
     private LocationDto location;
 
-    private Boolean paid;
 
-    private Integer participantLimit;
+    private Boolean paid = false;
 
-    private Boolean requestModeration;
 
-    private String title;
+    private Integer participantLimit = 0;
+
+
+    private Boolean requestModeration = true;
 }
